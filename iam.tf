0b367ffc5b71448029995c2292ad1b6abb8b155a
Moin5568 <ms889915@gmail.com>
2025-11-27 09:52:00 +0530

    new code is added

--- iam.tf
+++ iam.tf
@@ -1,25 +1,26 @@
 
 resource "aws_iam_role" "our-iam-role" {
-    name = "Terraform-Admin"
-    assume_role_policy = <<EOF  //<<EOF ek delimiter (marker) hai jo multi-line content ki shuruaat karta hai.
-{
-   "Version": "2012-10-17",        // JSON policy ka version
-   "Statement": [                 // Ek ya ek se zyada rules define karne ke liye
-     {
-       "Sid": "",                 // Statement ID (optional)
-       "Effect": "Allow",         // "Allow" action ko enable kar raha hai
-       "Principal": {             // Role ka user ya service kaun hai?
-         "Service": [             // AWS service define karte hain
-           "ec2.amazonaws.com"    // Sirf EC2 service ko ye role use karne ki permission di
-         ]
-       },
-       "Action": "sts:AssumeRole" // Role ko assume karne ka action define karta hai
-     }
-   ]
- }
+  name = "Terraform-Admin"
 
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": [
+          "ec2.amazonaws.com"
+        ]
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
 EOF
 }
+
   
 
 
